95c42d7a309c79ea52adc50ee267d23793a81c06
Luke Wheeler <115876478+ghossssst@users.noreply.github.com>
2024-05-22 21:36:07 +0100

    restyled content js

--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -1,13 +1,15 @@
-let notification = document.createElement('div');
-notification.style.position = 'fixed';
-notification.style.top = '10px';
-notification.style.right = '10px';
-notification.style.backgroundColor = '#44c767';
-notification.style.color = 'white';
-notification.style.padding = '10px';
-notification.style.zIndex = '10000';
-notification.style.borderRadius = '5px';
-notification.textContent = 'Page cache has been cleared!';
+let notification = document.createElement("div");
+notification.style.position = "fixed";
+notification.style.top = "10px";
+notification.style.right = "195px";
+notification.style.backgroundColor = "#f5f5f5";
+notification.style.color = "black";
+notification.style.fontFamily = "Courier New", "Courier", "monospace";
+notification.style.fontSize = "14px";
+notification.style.padding = "10px";
+notification.style.zIndex = "10000";
+notification.style.borderRadius = "0px";
+notification.textContent = "cache cleared";
 document.body.appendChild(notification);
 
 
